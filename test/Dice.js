372c5257d1a5601836ee9da881838d8df45d12bb
Johan Haneveld <johan.haneveld@gmail.com>
2018-06-30 19:11:47 +0200

    fix random function call, functions test coverage 100%

--- test/Dice.js
+++ test/Dice.js
@@ -1,7 +1,43 @@
 import Dice from './../src/Dice'
 import test from 'ava'
 
-test('create dice', t => {
+test('create 2 dice with 6 faces', t => {
   const dice = new Dice(2, 6)
   t.truthy(dice)
+})
+
+test('check amount of dice', t => {
+  const dice = new Dice(2, 6)
+  t.true(dice.dice === 2)
+})
+
+test('check amount of faces', t => {
+  const dice = new Dice(2, 6)
+  t.true(dice.dice === 2)
+})
+
+test('roll dice', t => {
+  const dice = new Dice(2, 6)
+  const result = dice.roll()
+  t.true(result > 0)
+})
+
+test('clear dice tray', t => {
+  const dice = new Dice(2, 6)
+  dice.roll()
+  dice.clearTray()
+  t.true(dice.result === 0)
+  t.true(dice.tray.length === 0)
+})
+
+test('check result', t => {
+  const dice = new Dice(2, 6)
+  dice.roll()
+  t.true(dice.result > 0)
+})
+
+test('check tray', t => {
+  const dice = new Dice(2, 6)
+  dice.roll()
+  t.true(dice.tray.length > 0)
 })
