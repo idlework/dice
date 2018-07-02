e503e69bd6236e1190d517b83f37f48de6b165fe
Johan Haneveld <johan.haneveld@gmail.com>
2018-07-02 15:03:50 +0200

    add documentation to code

--- src/Dice.js
+++ src/Dice.js
@@ -1,9 +1,20 @@
 module.exports = class Dice {
+  /**
+   * Intitialize Dice class.
+   *
+   * @param {Number} dice - Number corresponding with the amount of dice.
+   * @param {Number} faces - Number corresponding with the amount of faces on the dice.
+   */
   constructor (dice, faces) {
     this._validateConstructor(dice, faces)
     this._initConstructor(dice, faces)
   }
 
+  /**
+   * Roll the dice.
+   *
+   * @return {Array} - Returns the total of all dice.
+   */
   roll () {
     this.clearTray()
     do {
@@ -15,23 +26,46 @@ module.exports = class Dice {
     return this.result
   }
 
+  /**
+   * Clears the tray from dice. Result and tray will be cleared.
+   */
   clearTray () {
     this._result = 0
     this.tray.splice(0, this.tray.length)
   }
 
+  /**
+   * Return the amount of dice.
+   *
+   * @return {Number} - Return the amount of dice.
+   */
   get dice () {
     return this._dice
   }
 
+  /**
+   * Returns the amount of faces on a the dice.
+   *
+   * @return {Number} - Returns the amount of faces on a the dice.
+   */
   get faces () {
     return this._faces
   }
 
+  /**
+   * Returns the total of all the dice.
+   *
+   * @return {Number} - Returns the total of all the dice.
+   */
   get result () {
     return this._result
   }
 
+  /**
+   * Returns the tray(array) with all the dice.
+   *
+   * @return {Array} - Return an array with all the dice.
+   */
   get tray () {
     return this._tray
   }
